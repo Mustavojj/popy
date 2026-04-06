da24bfd4409e325fb100de60d7f76f5a4f393c7d
Mustavojj <mostavojj@gmail.com>
2026-04-06 19:09:08 +0300

    Create check-bot-admin.js

---
+++ api/check-bot-admin.js
@@ -0,0 +1,8 @@
+export default async function handler(req, res) {
+    const { chatId } = req.body;
+    const BOT_TOKEN = process.env.BOT_TOKEN;
+    const response = await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/getChatAdministrators?chat_id=${chatId}`);
+    const data = await response.json();
+    const isAdmin = data.result?.some(admin => admin.user?.is_bot && admin.user?.username === 'Popbuzbot');
+    res.json({ isAdmin });
+}
