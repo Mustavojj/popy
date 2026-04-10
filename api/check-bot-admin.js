aa5f3cbf28e4d0f81b64acba7a0b27f40bd3eba1
Mustavojj <mostavojj@gmail.com>
2026-04-10 19:35:46 +0300

    Update check-bot-admin.js

--- api/check-bot-admin.js
+++ api/check-bot-admin.js
@@ -3,6 +3,6 @@ export default async function handler(req, res) {
     const BOT_TOKEN = process.env.BOT_TOKEN;
     const response = await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/getChatAdministrators?chat_id=${chatId}`);
     const data = await response.json();
-    const isAdmin = data.result?.some(admin => admin.user?.is_bot && admin.user?.username === 'Popbuzbot');
+    const isAdmin = data.result?.some(admin => admin.user?.is_bot && admin.user?.username === 'Strzzbot');
     res.json({ isAdmin });
 }
