6d8dffe2da6ab4ecc196220a07df82e09646d155
Mustavojj <mostavojj@gmail.com>
2026-04-26 23:05:11 +0300

    Delete api/verify-member.js

--- api/verify-member.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-    const { chatId, userId } = req.body;
-    const BOT_TOKEN = process.env.BOT_TOKEN;
-    const response = await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/getChatMember?chat_id=${chatId}&user_id=${userId}`);
-    const data = await response.json();
-    const isMember = ['member', 'administrator', 'creator'].includes(data.result?.status);
-    res.json({ isMember });
-}
