e382d26605fa4eae7647e466810b546bdcebac23
Mustavojj <mostavojj@gmail.com>
2026-04-26 23:03:45 +0300

    Delete api/check-bot-admin.js

--- api/check-bot-admin.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-    const { chatId } = req.body;
-    const BOT_TOKEN = process.env.BOT_TOKEN;
-    const response = await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/getChatAdministrators?chat_id=${chatId}`);
-    const data = await response.json();
-    const isAdmin = data.result?.some(admin => admin.user?.is_bot && admin.user?.username === 'Strzzbot');
-    res.json({ isAdmin });
-}
