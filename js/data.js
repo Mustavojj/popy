ce121f99dd0988a3bd8df7f6d4dad84ebdbcea02
Mustavojj <mostavojj@gmail.com>
2026-05-19 23:20:05 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -13,7 +13,7 @@ export const APP_CONFIG = {
     BOT_AVATAR: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg",
     LEVEL_FORMULA: { base: 1000, multiplier: 1.2 },
     AD_COOLDOWN_HOURS: 3,
-    REWARD_AD_BLOCK_ID: "30072",
-    INTERSTITIAL_AD_BLOCK_ID: "int-30073",
+    REWARD_AD_BLOCK_ID: "30627",
+    INTERSTITIAL_AD_BLOCK_ID: "int-30628",
     BOT_LINK: "https://t.me/Veltrxbot/velt?startapp="
 };
