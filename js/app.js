2f430cf57118b9dcf9d5b2850195b4c0553dabc7
Mustavojj <mostavojj@gmail.com>
2026-05-27 00:26:27 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1536,7 +1536,7 @@ async completeDailyAdTask() {
                         <div class="daily-task-icon"><i class="fas fa-newspaper"></i></div>
                         <div class="daily-task-info">
                             <h4>${this.t('daily_check_news')}</h4>
-                            <div class="daily-task-reward">+10 ${this.t('power')}</div>
+                            <i class="fas fa-bolt"></i><div class="daily-task-reward"> 10 ${this.t('power')}</div>
                         </div>
                         <button class="task-btn ${dailyCheckNewsBtnClass}" id="daily-check-news-btn" ${dailyCheckNewsBtnDisabled ? 'disabled' : ''}>${dailyCheckNewsBtnText}</button>
                     </div>
