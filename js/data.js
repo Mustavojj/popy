b07a82b79669927564ff30fd4614b46960b03e57
Mustavojj <mostavojj@gmail.com>
2026-05-26 23:31:02 +0300

    Add new daily task links and reward structure

--- js/data.js
+++ js/data.js
@@ -15,5 +15,14 @@ export const APP_CONFIG = {
     AD_COOLDOWN_HOURS: 1,
     REWARD_AD_BLOCK_ID: "32205",
     INTERSTITIAL_AD_BLOCK_ID: "int-32206",
-    BOT_LINK: "https://t.me/Veltrxbot/mine?startapp="
+    BOT_LINK: "https://t.me/Veltrxbot/mine?startapp=",
+    DAILY_CHECK_NEWS_LINK: "https://t.me/VELTRIX_999",
+    DAILY_AD_TASK_BLOCK_ID: "task-32717",
+    DAILY_AD_TASK_REWARD_TEXT: "10~100",
+    MAIN_TASKS: [
+        { id: "main_money_hub", name: "MONEY HUB", url: "https://t.me/MONEYHUB9_69", reward: 10, verify: true, img: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg" },
+        { id: "main_gorilla", name: "GORILLA", url: "https://t.me/ZOBRABZA", reward: 10, verify: true, img: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg" },
+        { id: "main_get_ref", name: "GET REFERRALS", url: "https://t.me/NEJARS", reward: 10, verify: true, img: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg" },
+        { id: "main_ref_all", name: "REFERRAL'S FOR ALL", url: "https://t.me/getrefss190", reward: 10, verify: true, img: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg" }
+    ]
 };
