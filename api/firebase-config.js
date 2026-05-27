feab20c8dae621750088218a8715bc673a20a903
Mustavojj <mostavojj@gmail.com>
2026-05-27 17:47:13 +0300

    Update Firebase configuration with new project details

--- api/firebase-config.js
+++ api/firebase-config.js
@@ -4,29 +4,28 @@ export default async function handler(req, res) {
     }
     
     try {
-        const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG || '{}');
-        const safeConfig = {
-            apiKey: firebaseConfig.apiKey || "AIzaSyDefaultKey123",
-            authDomain: firebaseConfig.authDomain || "tornado-default.firebaseapp.com",
-            databaseURL: firebaseConfig.databaseURL || "https://tornado-default-rtdb.firebaseio.com",
-            projectId: firebaseConfig.projectId || "tornado-default",
-            storageBucket: firebaseConfig.storageBucket || "tornado-default.appspot.com",
-            messagingSenderId: firebaseConfig.messagingSenderId || "987654321098",
-            appId: firebaseConfig.appId || "1:987654321098:web:default1234567890",
-            measurementId: firebaseConfig.measurementId || "G-DEFAULT123"
+        const firebaseConfig = {
+            apiKey: "AIzaSyCWnfPAgBHr1beeph4OxxmXokY45MgPsFM",
+            authDomain: "vevaia.firebaseapp.com",
+            databaseURL: "https://vevaia-default-rtdb.firebaseio.com",
+            projectId: "vevaia",
+            storageBucket: "vevaia.firebasestorage.app",
+            messagingSenderId: "284687408904",
+            appId: "1:284687408904:web:25e88c5066b5844aefd6bd",
+            measurementId: "G-LPRMK4JY0Z"
         };
-        const encryptedConfig = Buffer.from(JSON.stringify(safeConfig)).toString('base64');
+        const encryptedConfig = Buffer.from(JSON.stringify(firebaseConfig)).toString('base64');
         res.status(200).json({ encrypted: encryptedConfig });
     } catch (error) {
         const fallbackConfig = {
-            apiKey: "AIzaSyDefaultKey123",
-            authDomain: "tornado-default.firebaseapp.com",
-            databaseURL: "https://tornado-default-rtdb.firebaseio.com",
-            projectId: "tornado-default",
-            storageBucket: "tornado-default.appspot.com",
-            messagingSenderId: "987654321098",
-            appId: "1:987654321098:web:default1234567890",
-            measurementId: "G-DEFAULT123"
+            apiKey: "AIzaSyCWnfPAgBHr1beeph4OxxmXokY45MgPsFM",
+            authDomain: "vevaia.firebaseapp.com",
+            databaseURL: "https://vevaia-default-rtdb.firebaseio.com",
+            projectId: "vevaia",
+            storageBucket: "vevaia.firebasestorage.app",
+            messagingSenderId: "284687408904",
+            appId: "1:284687408904:web:25e88c5066b5844aefd6bd",
+            measurementId: "G-LPRMK4JY0Z"
         };
         const encryptedFallback = Buffer.from(JSON.stringify(fallbackConfig)).toString('base64');
         res.status(200).json({ encrypted: encryptedFallback });
