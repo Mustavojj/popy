18febb0b1ab9ae3e0fd1252635c315bedb8cafb3
Mustavojj <mostavojj@gmail.com>
2026-05-28 17:58:29 +0300

    Change AD cooldown from 3 to 6 hours

--- js/data.js
+++ js/data.js
@@ -12,7 +12,7 @@ export const APP_CONFIG = {
     DEFAULT_USER_AVATAR: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg",
     BOT_AVATAR: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg",
     LEVEL_FORMULA: { base: 1000, multiplier: 1.2 },
-    AD_COOLDOWN_HOURS: 3,
+    AD_COOLDOWN_HOURS: 6,
     REWARD_AD_BLOCK_ID: "32205",
     INTERSTITIAL_AD_BLOCK_ID: "int-32206",
     BOT_LINK: "https://t.me/Veltrxbot/mine?startapp=",
