d56ea82e270e76f464334036f2cef97244b16128
Mustavojj <mostavojj@gmail.com>
2026-06-03 18:59:09 +0300

    Update MAIN_TASKS with new entries and IDs

--- js/data.js
+++ js/data.js
@@ -14,9 +14,10 @@ export const APP_CONFIG = {
     BOT_LINK: "https://t.me/Veltrxbot/mine?startapp=",
     DAILY_CHECK_NEWS_LINK: "https://t.me/VELTRIX_999",
     MAIN_TASKS: [
-        { id: "main_money_hub", name: "MONEY HUB", url: "https://t.me/MONEYHUB9_69", reward: 10, verify: true, img: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg" },
-        { id: "main_gorilla", name: "GORILLA", url: "https://t.me/ZOBRABZA", reward: 10, verify: true, img: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg" },
-        { id: "main_get_ref", name: "GET REFERRALS", url: "https://t.me/NEJARS", reward: 10, verify: true, img: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg" },
-        { id: "main_ref_all", name: "REFERRAL'S FOR ALL", url: "https://t.me/getrefss190", reward: 10, verify: false, img: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg" }
+        { id: "money_hub", name: "MONEY HUB", url: "https://t.me/MONEYHUB9_69", reward: 10, verify: true, img: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg" },
+        { id: "ton_labs", name: "TON LABS", url: "https://t.me/TonLabstg", reward: 10, verify: true, img: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg" },
+        { id: "gorilla", name: "GORILLA", url: "https://t.me/ZOBRABZA", reward: 10, verify: true, img: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg" },
+        { id: "get_ref", name: "GET REFERRALS", url: "https://t.me/NEJARS", reward: 10, verify: true, img: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg" },
+        { id: "ref_all", name: "REFERRALS FOR ALL", url: "https://t.me/getrefss190", reward: 10, verify: false, img: "https://i.ibb.co/B5z8x1vJ/afb301036766.jpg" }
     ]
 };
